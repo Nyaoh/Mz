0350f4ad8743fe87a9287464ed2c1d6500cb8d34
AMORIN <AMORIN@AMORIN.KLEE.LAN.NET>
2015-03-13 11:22:10 +0100

    V0.0.1 Nettoyage application.ini Ajout contrôleur de test "UtilisateurController"

--- webapp/application/modules/default/controllers/IndexController.php
+++ webapp/application/modules/default/controllers/IndexController.php
@@ -3,14 +3,5 @@
 class IndexController extends Zend_Controller_Action
 {
 	public function indexAction() {
-		
-// 		$service = new Default_Service_FamilleTypePost();
-		
-// 		$this->view->ftpList = $service->getList();
-		
-		$this->view->foo = '\'Hello world !\'';
-		
-		$model = new Default_Service_Register();
-		$this->view->form = new Default_Form_Register();
 	}
 }
---
+++ webapp/application/modules/default/controllers/UtilisateurController.php
@@ -0,0 +1,8 @@
+<?php 
+
+class UtilisateurController extends Zend_Controller_Action
+{
+	public function indexAction() {
+		
+	}
+}
